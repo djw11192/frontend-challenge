06fa851081a483e1e6af82e935f487f88c9c3541
David Weber <davidjwe33@gmail.com>
2017-10-02 07:24:15 -0700

    changed README to instruct how to run angular-petlist

--- angular-petlist/index.js
+++ angular-petlist/index.js
@@ -14,9 +14,6 @@ app.use(express.static('client'))
 app.use(bodyParser.json())
 
 app.use('/', primaryRouter)
-/*app.get('/', function(req, res) {
-  res.sendFile('client/index.html', {root: '/'})
-})*/
 
 app.listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
