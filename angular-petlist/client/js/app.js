629008aa03f3dbafae65135f23b629e85b196775
David Weber <davidjwe33@gmail.com>
2017-10-01 21:27:07 -0700

    completed url function

--- angular-petlist/client/js/app.js
+++ angular-petlist/client/js/app.js
@@ -30,8 +30,19 @@ angular.module('petlist', ['ui.router', 'petlist.services'])
     init = function(){
       $http.get('/web-api/search.json')
         .then(function(response){
-          PetFactory.setUrl(response);
+          setUrl(response)
+          setName(response)
         })
+    }
+
+    function setUrl(listData){
+      var searchArr = listData.data.search;
+      searchArr.forEach(function(listItem){
+        listItem.title = listItem.title.trim()//trim takes away and leading or ending spaces
+          .replace(/[^a-zA-Z0-9\s]/g, '')//take out non-alpha numeric characters
+          .replace(/\s+/g, '-')//replace spaces with dashes
+          .toLowerCase()
+      })
     }
 
     init();
--- angular-petlist/client/js/services.js
+++ angular-petlist/client/js/services.js
@@ -9,11 +9,11 @@ function PetFactory($http){
   function setUrl(listData){
     var searchArr = listData.data.search;
     searchArr.forEach(function(listItem){
-      var url = listItem.title.trim()
-        .replace(/[^a-zA-Z0-9\s]/g, '')
-        .replace(/\s+/g, '-')
+      listItem.title = listItem.title.trim()//trim takes away and leading or ending spaces
+        .replace(/[^a-zA-Z0-9\s]/g, '')//take out non-alpha numeric characters
+        .replace(/\s+/g, '-')//replace spaces with dashes
         .toLowerCase()
-      console.log(url)
     })
+    //return searchArr;
   }
 }
